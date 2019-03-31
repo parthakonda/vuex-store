a6e3f7ba61e526c1a1fa9b2e4a2e109b084a2154
parthz <parthsaradhi1992@gmail.com>
2019-04-01 05:13:34 +0530

    added state management

--- vuestate/src/store.js
+++ vuestate/src/store.js
@@ -4,7 +4,26 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    title: "Awesome Title",
+    notifications: [
+      {
+        message: "Record Saved Successfully",
+        type: "SUCCESS",
+        id: 1
+      },
+      {
+        message: "Invalid Record Data",
+        type: "FAILED",
+        id: 2
+      },
+      {
+        message: "Content Parsing Failed",
+        type: "WARNING",
+        id: 3
+      }
+    ]
+  },
   mutations: {},
   actions: {}
 });
